27166e4b1bcd9b43d85b288fb2621fa321a28581
Shisham Chudal <shishamchudal61@gmail.com>
2025-01-31 10:37:31 -0800

    Refactor random code generation and enhance button disable logic with input evaluation

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,6 +1,9 @@
+var code = "";
+var getCode = '';
+var btnValue;
+var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+
 function generateCode() {
-  var code = "";
-  var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
 
   for (var i = 1; i <= 8; i++) {
     var char = Math.random() * str.length;
@@ -12,9 +15,26 @@ function generateCode() {
 
 document.getElementById("codes").innerHTML = generateCode();
 
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+function disableButton(btnValue) {
+    document.getElementById("submit").disabled = btnValue;
+    if (btnValue == true) {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
 }
 
-disableButton();
+var codebox = document.getElementById("codeentered");
+codebox.addEventListener("input", evaluateCode);
 
+function evaluateCode() {
+    getCode = document.getElementById("codeentered").value;
+    var charset1 = getCode.trim();
+    var charset2 = code.trim();
+
+    if (charset1.length == charset2.length && charset1 == charset2) {
+        disableButton(false);
+    }
+}
